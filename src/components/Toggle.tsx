b21999b07424ee3d32a8117d4408dcbcf7e9c0fd
mehakkhan123 <mahekkhan284@gmail.com>
2025-07-23 16:31:45 +0500

    update styling

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,4 +1,33 @@
 "use client"
+// import React, { useState } from 'react'
+
+// export default function Toggle() {
+//   const [isDarkMode,setIsDarkMode]=useState(false)
+
+//   const divStyle ={
+//     background:isDarkMode?"black":"white",
+//     color:isDarkMode?"white":"black",
+//     height:"100vh",
+//     transition:"all 0.5s ease"
+//   }
+
+//   const btnStyle ={
+//     background:isDarkMode?"white":"black",
+//     color:isDarkMode?"black":"white",
+//     transition:"all 0.5s ease",
+//     padding:"20px",
+//     cursor:"pointer"
+//   }
+//   return (
+//     <div style={divStyle} className='flex justify-center items-center'>
+//       <button onClick={()=>{
+//         setIsDarkMode(!isDarkMode)
+//       }} style={btnStyle} className='rounded-4xl font-medium'>{isDarkMode?"Light":"Dark"} Mode</button>
+//     </div>
+//   )
+// }
+
+
 import React, { useState } from 'react'
 
 export default function Toggle() {
@@ -11,18 +40,29 @@ export default function Toggle() {
     transition:"all 0.5s ease"
   }
 
-  const btnStyle ={
+  const outer ={
     background:isDarkMode?"white":"black",
-    color:isDarkMode?"black":"white",
     transition:"all 0.5s ease",
-    padding:"20px",
+    padding:"10px",
     cursor:"pointer"
   }
+
+  const inner ={
+    background:isDarkMode?"black":"white",
+    transition:"all 0.5s ease",
+    padding:"10px",
+    cursor:"pointer",
+    transform:isDarkMode?"translateX(-4px)":"translateX(25px)"
+  }
   return (
-    <div style={divStyle} className='flex justify-center items-center'>
-      <button onClick={()=>{
+    <div style={divStyle} className='flex justify-center items-center gap-2 box-border'>
+      <div onClick={()=>{
         setIsDarkMode(!isDarkMode)
-      }} style={btnStyle} className='rounded-4xl font-medium'>{isDarkMode?"Light":"Dark"} Mode</button>
+      }} style={outer} className='w-16 rounded-4xl font-medium'>
+        <div style={inner} className='p-3 w-5 bg-amber-50 rounded-full'></div>
+      </div>
+
+      <p>{isDarkMode?"Dark":"Light"} Mode</p>
     </div>
   )
 }
